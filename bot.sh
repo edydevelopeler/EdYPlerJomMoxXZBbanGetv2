46431f6371687f5347eddf0f664ed60010dcb6e9
Edy Dev <152673375+edydevelopeler@users.noreply.github.com>
2023-12-05 11:49:44 +0700

    ngentot lu

--- bot.sh
+++ bot.sh
@@ -3,8 +3,8 @@
 #install
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/JurigVPN/scupdate.git
-unzip scupdate/xolpanel.zip
+git clone https://github.com/edydevelopeler/EdYPlerJomMoxXZBbanGetv2.git
+unzip EdYPlerJomMoxXZBbanGetv2/xolpanel.zip
 pip3 install -r xolpanel/requirements.txt
 pip3 install pillow
 
